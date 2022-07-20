5d49fc84197b81e8e066bd74243922f6e7a22c95
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2022-07-20 20:23:44 -0300

    first results

--- src/execute.sh
+++ src/execute.sh
@@ -1,17 +1,66 @@
+# epoch=100
+# models=("regnet" "mobile" "shuffle" "efficient" "shuffle")
+# optims=("adam" "sgd" "radam" "ranger")
+# k_fold=5
+# batch_size=32
+
+# for (( j=0; j<${#models[@]} ; j+=1 )) ; do
+#     for (( i=0; i<${#optims[@]} ; i+=2 )) ; do
+#         echo "${models[j]} + ${optims[i]} + ${optims[i+1]}"
+#         python main.py --optim ${optims[i]} --batch_size ${batch_size} --k_fold ${k_fold}\
+#         --model_name ${models[j]} &
+#         python main.py --optim ${optims[i+1]} --batch_size ${batch_size} --k_fold ${k_fold}\
+#         --model_name ${models[j]} &
+
+#         wait
+#     done
+# done
+
+# epoch=100
+# models=("regnet" "mobile" "shuffle" "efficient" "shuffle")
+# optims=("adam" "sgd" "radam" "ranger")
+# k_fold=5
+# batch_size=32
+# lr=0.001
+
+# for (( j=0; j<${#models[@]} ; j+=1 )) ; do
+#     for (( i=0; i<${#optims[@]} ; i+=2 )) ; do
+#         echo "${models[j]} + ${optims[i]} + ${optims[i+1]}"
+#         python main.py --optim ${optims[i]} --batch_size ${batch_size} --k_fold ${k_fold}\
+#         --model_name ${models[j]} --lr ${lr} &
+#         python main.py --optim ${optims[i+1]} --batch_size ${batch_size} --k_fold ${k_fold}\
+#         --model_name ${models[j]} --lr ${lr} &
+
+#         wait
+#     done
+# done
+
+
 epoch=100
-models=("regnet" "mobile" "shuffle" "efficient" "shuffle")
+models=("regnet")
 optims=("adam" "sgd" "radam" "ranger")
 k_fold=5
 batch_size=32
+lr=0.001
 
 for (( j=0; j<${#models[@]} ; j+=1 )) ; do
     for (( i=0; i<${#optims[@]} ; i+=2 )) ; do
         echo "${models[j]} + ${optims[i]} + ${optims[i+1]}"
         python main.py --optim ${optims[i]} --batch_size ${batch_size} --k_fold ${k_fold}\
-        --model_name ${models[j]} &
+        --model_name ${models[j]} --lr ${lr} --multi_input &
         python main.py --optim ${optims[i+1]} --batch_size ${batch_size} --k_fold ${k_fold}\
-        --model_name ${models[j]} &
+        --model_name ${models[j]} --lr ${lr} --multi_input &
 
         wait
     done
 done
+
+for (( j=0; j<${#models[@]} ; j+=1 )) ; do
+    for (( i=0; i<${#optims[@]} ; i+=1 )) ; do
+        echo "${models[j]} + ${optims[i]} + ${optims[i+1]}"
+        python main.py --optim ${optims[i]} --batch_size ${batch_size} --k_fold ${k_fold}\
+        --model_name ${models[j]} --lr ${lr} --multi_input --double_img &
+        wait
+    done
+done
+
