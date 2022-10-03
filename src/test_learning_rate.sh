bcaa8a315f39bae483093761329e2c410d6cb462
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2022-10-03 09:57:30 -0300

    learning rate script

--- src/test_learning_rate.sh
+++ src/test_learning_rate.sh
@@ -7,12 +7,15 @@ early_start=100
 output_tab=5
 
 for (( j=0; j<${#models[@]} ; j+=1 )) ; do
-    for (( i=0; i<${#optims[@]} ; i+=2 )) ; do
+    for (( i=0; i<${#optims[@]} ; i+=4 )) ; do
         for (( k=0; k<${#lr[@]} ; k+=1 )) ; do
-            echo "${models[j]} + ${optims[i]} + ${optims[i+1]} + ${lr[k]}"
             python main.py --optim ${optims[i]} --batch_size ${batch_size} \
             --model_name ${models[j]} --epochs ${epoch} --early_start ${early_start} --lr ${lr} &
             python main.py --optim ${optims[i+1]} --batch_size ${batch_size} \
+            --model_name ${models[j]} --epochs ${epoch} --early_start ${early_start} --lr ${lr} &
+            python main.py --optim ${optims[i+2]} --batch_size ${batch_size} \
+            --model_name ${models[j]} --epochs ${epoch} --early_start ${early_start} --lr ${lr} &
+            python main.py --optim ${optims[i+3]} --batch_size ${batch_size} \
             --model_name ${models[j]} --epochs ${epoch} --early_start ${early_start} --lr ${lr} &
 
         wait
@@ -20,17 +23,21 @@ for (( j=0; j<${#models[@]} ; j+=1 )) ; do
     done
 done
 
-
 for (( j=0; j<${#models[@]} ; j+=1 )) ; do
-    for (( i=0; i<${#optims[@]} ; i+=2 )) ; do
+    for (( i=0; i<${#optims[@]} ; i+=4 )) ; do
         for (( k=0; k<${#lr[@]} ; k+=1 )) ; do
-            echo "${models[j]} + ${optims[i]} + ${optims[i+1]} + ${lr[k]}"
             python main.py --optim ${optims[i]} --batch_size ${batch_size} \
             --model_name ${models[j]} --epochs ${epoch} --early_start ${early_start} --double_img \
             --lr ${lr} &
             python main.py --optim ${optims[i+1]} --batch_size ${batch_size} \
             --model_name ${models[j]} --epochs ${epoch} --early_start ${early_start} --double_img \
             --lr ${lr} &
+            python main.py --optim ${optims[i+2]} --batch_size ${batch_size} \
+            --model_name ${models[j]} --epochs ${epoch} --early_start ${early_start} --double_img \
+            --lr ${lr} &
+            python main.py --optim ${optims[i+3]} --batch_size ${batch_size} \
+            --model_name ${models[j]} --epochs ${epoch} --early_start ${early_start} --double_img \
+            --lr ${lr} &
         wait
         done
     done
@@ -39,28 +46,36 @@ done
 for (( j=0; j<${#models[@]} ; j+=1 )) ; do
     for (( i=0; i<${#optims[@]} ; i+=2 )) ; do
         for (( k=0; k<${#lr[@]} ; k+=1 )) ; do
-            echo "${models[j]} + ${optims[i]} + ${optims[i+1]} + ${lr[k]}"
             python main.py --optim ${optims[i]} --batch_size ${batch_size} \
             --model_name ${models[j]} --epochs ${epoch} --early_start ${early_start} \
             --output_tab ${output_tab} --lr ${lr} &
             python main.py --optim ${optims[i+1]} --batch_size ${batch_size} \
             --model_name ${models[j]} --epochs ${epoch} --early_start ${early_start} \
             --output_tab ${output_tab} --lr ${lr} &
+            python main.py --optim ${optims[i+2]} --batch_size ${batch_size} \
+            --model_name ${models[j]} --epochs ${epoch} --early_start ${early_start} \
+            --output_tab ${output_tab} --lr ${lr} &
+            python main.py --optim ${optims[i+3]} --batch_size ${batch_size} \
+            --model_name ${models[j]} --epochs ${epoch} --early_start ${early_start} \
+            --output_tab ${output_tab} --lr ${lr} &
         wait
         done
     done
 done
 
-
-
 for (( j=0; j<${#models[@]} ; j+=1 )) ; do
     for (( i=0; i<${#optims[@]} ; i+=2 )) ; do
         for (( k=0; k<${#lr[@]} ; k+=1 )) ; do
-            echo "${models[j]} + ${optims[i]} + ${optims[i+1]} + ${lr[k]}"
             python main.py --optim ${optims[i]} --batch_size ${batch_size} \
             --model_name ${models[j]} --epochs ${epoch} --early_start ${early_start} --output_tab \
                     ${output_tab} --double_img --lr ${lr} &
             python main.py --optim ${optims[i+1]} --batch_size ${batch_size} \
+            --model_name ${models[j]} --epochs ${epoch} --early_start ${early_start} --output_tab \
+                    ${output_tab} --double_img --lr ${lr} &
+            python main.py --optim ${optims[i+2]} --batch_size ${batch_size} \
+            --model_name ${models[j]} --epochs ${epoch} --early_start ${early_start} --output_tab \
+                    ${output_tab} --double_img --lr ${lr} &
+            python main.py --optim ${optims[i+3]} --batch_size ${batch_size} \
             --model_name ${models[j]} --epochs ${epoch} --early_start ${early_start} --output_tab \
                     ${output_tab} --double_img --lr ${lr} &
         wait
