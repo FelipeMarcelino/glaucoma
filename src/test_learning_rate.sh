4f73478711444cd253b33b9d2f2b0610df4b1e11
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2022-11-15 21:13:26 -0300

    fix test learning rate

--- src/test_learning_rate.sh
+++ src/test_learning_rate.sh
@@ -2,7 +2,7 @@ epoch=100
 models=("regnet" "mobile" "shuffle" "efficient" "regnet16" "regnet32" "inception")
 optims=("adam" "sgd" "radam" "ranger")
 lr=(0.01 0.001 0.0001 0.0005)
-eatch_size=16
+batch_size=16
 early_start=100
 output_tab=5
 
@@ -24,68 +24,68 @@ for (( j=0; j<${#models[@]} ; j+=1 )) ; do
     done
 done
 
-echo "Double image"
-for (( j=0; j<${#models[@]} ; j+=1 )) ; do
-    echo "model: ${models[j]}"
-    for (( i=0; i<${#optims[@]} ; i+=4 )) ; do
-        for (( k=0; k<${#lr[@]} ; k+=1 )) ; do
-            python main.py --optim ${optims[i]} --batch_size ${batch_size} \
-            --backbone ${models[j]} --epochs ${epoch} --early_start ${early_start} --double_img \
-            --lr ${lr[k]} &
-            python main.py --optim ${optims[i+1]} --batch_size ${batch_size} \
-            --backbone ${models[j]} --epochs ${epoch} --early_start ${early_start} --double_img \
-            --lr ${lr[k]} &
-            python main.py --optim ${optims[i+2]} --batch_size ${batch_size} \
-            --backbone ${models[j]} --epochs ${epoch} --early_start ${early_start} --double_img \
-            --lr ${lr[k]} &
-            python main.py --optim ${optims[i+3]} --batch_size ${batch_size} \
-            --backbone ${models[j]} --epochs ${epoch} --early_start ${early_start} --double_img \
-            --lr ${lr[k]} &
-        wait
-        done
-    done
-done
+# echo "Double image"
+# for (( j=0; j<${#models[@]} ; j+=1 )) ; do
+#     echo "model: ${models[j]}"
+#     for (( i=0; i<${#optims[@]} ; i+=4 )) ; do
+#         for (( k=0; k<${#lr[@]} ; k+=1 )) ; do
+#             python main.py --optim ${optims[i]} --batch_size ${batch_size} \
+#             --backbone ${models[j]} --epochs ${epoch} --early_start ${early_start} --double_img \
+#             --lr ${lr[k]} &
+#             python main.py --optim ${optims[i+1]} --batch_size ${batch_size} \
+#             --backbone ${models[j]} --epochs ${epoch} --early_start ${early_start} --double_img \
+#             --lr ${lr[k]} &
+#             python main.py --optim ${optims[i+2]} --batch_size ${batch_size} \
+#             --backbone ${models[j]} --epochs ${epoch} --early_start ${early_start} --double_img \
+#             --lr ${lr[k]} &
+#             python main.py --optim ${optims[i+3]} --batch_size ${batch_size} \
+#             --backbone ${models[j]} --epochs ${epoch} --early_start ${early_start} --double_img \
+#             --lr ${lr[k]} &
+#         wait
+#         done
+#     done
+# done
 
-echo "One image and tab"
-for (( j=0; j<${#models[@]} ; j+=1 )) ; do
-    echo "model: ${models[j]}"
-    for (( i=0; i<${#optims[@]} ; i+=4 )) ; do
-        for (( k=0; k<${#lr[@]} ; k+=1 )) ; do
-            python main.py --optim ${optims[i]} --batch_size ${batch_size} \
-            --backbone ${models[j]} --epochs ${epoch} --early_start ${early_start} \
-            --output_tab ${output_tab} --lr ${lr[k]} &
-            python main.py --optim ${optims[i+1]} --batch_size ${batch_size} \
-            --backbone ${models[j]} --epochs ${epoch} --early_start ${early_start} \
-            --output_tab ${output_tab} --lr ${lr[k]} &
-            python main.py --optim ${optims[i+2]} --batch_size ${batch_size} \
-            --backbone ${models[j]} --epochs ${epoch} --early_start ${early_start} \
-            --output_tab ${output_tab} --lr ${lr[k]} &
-            python main.py --optim ${optims[i+3]} --batch_size ${batch_size} \
-            --backbone ${models[j]} --epochs ${epoch} --early_start ${early_start} \
-            --output_tab ${output_tab} --lr ${lr[k]} &
-        wait
-        done
-    done
-done
+# echo "One image and tab"
+# for (( j=0; j<${#models[@]} ; j+=1 )) ; do
+#     echo "model: ${models[j]}"
+#     for (( i=0; i<${#optims[@]} ; i+=4 )) ; do
+#         for (( k=0; k<${#lr[@]} ; k+=1 )) ; do
+#             python main.py --optim ${optims[i]} --batch_size ${batch_size} \
+#             --backbone ${models[j]} --epochs ${epoch} --early_start ${early_start} \
+#             --output_tab ${output_tab} --lr ${lr[k]} &
+#             python main.py --optim ${optims[i+1]} --batch_size ${batch_size} \
+#             --backbone ${models[j]} --epochs ${epoch} --early_start ${early_start} \
+#             --output_tab ${output_tab} --lr ${lr[k]} &
+#             python main.py --optim ${optims[i+2]} --batch_size ${batch_size} \
+#             --backbone ${models[j]} --epochs ${epoch} --early_start ${early_start} \
+#             --output_tab ${output_tab} --lr ${lr[k]} &
+#             python main.py --optim ${optims[i+3]} --batch_size ${batch_size} \
+#             --backbone ${models[j]} --epochs ${epoch} --early_start ${early_start} \
+#             --output_tab ${output_tab} --lr ${lr[k]} &
+#         wait
+#         done
+#     done
+# done
 
-echo "Double image and tab"
-for (( j=0; j<${#models[@]} ; j+=1 )) ; do
-    echo "model: ${models[j]}"
-    for (( i=0; i<${#optims[@]} ; i+=4 )) ; do
-        for (( k=0; k<${#lr[@]} ; k+=1 )) ; do
-            python main.py --optim ${optims[i]} --batch_size ${batch_size} \
-            --backbone ${models[j]} --epochs ${epoch} --early_start ${early_start} --output_tab \
-                    ${output_tab} --double_img --lr ${lr[k]} &
-            python main.py --optim ${optims[i+1]} --batch_size ${batch_size} \
-            --backbone ${models[j]} --epochs ${epoch} --early_start ${early_start} --output_tab \
-                    ${output_tab} --double_img --lr ${lr[k]} &
-            python main.py --optim ${optims[i+2]} --batch_size ${batch_size} \
-            --backbone ${models[j]} --epochs ${epoch} --early_start ${early_start} --output_tab \
-                    ${output_tab} --double_img --lr ${lr[k]} &
-            python main.py --optim ${optims[i+3]} --batch_size ${batch_size} \
-            --backbone ${models[j]} --epochs ${epoch} --early_start ${early_start} --output_tab \
-                    ${output_tab} --double_img --lr ${lr[k]} &
-        wait
-        done
-    done
-done
+# echo "Double image and tab"
+# for (( j=0; j<${#models[@]} ; j+=1 )) ; do
+#     echo "model: ${models[j]}"
+#     for (( i=0; i<${#optims[@]} ; i+=4 )) ; do
+#         for (( k=0; k<${#lr[@]} ; k+=1 )) ; do
+#             python main.py --optim ${optims[i]} --batch_size ${batch_size} \
+#             --backbone ${models[j]} --epochs ${epoch} --early_start ${early_start} --output_tab \
+#                     ${output_tab} --double_img --lr ${lr[k]} &
+#             python main.py --optim ${optims[i+1]} --batch_size ${batch_size} \
+#             --backbone ${models[j]} --epochs ${epoch} --early_start ${early_start} --output_tab \
+#                     ${output_tab} --double_img --lr ${lr[k]} &
+#             python main.py --optim ${optims[i+2]} --batch_size ${batch_size} \
+#             --backbone ${models[j]} --epochs ${epoch} --early_start ${early_start} --output_tab \
+#                     ${output_tab} --double_img --lr ${lr[k]} &
+#             python main.py --optim ${optims[i+3]} --batch_size ${batch_size} \
+#             --backbone ${models[j]} --epochs ${epoch} --early_start ${early_start} --output_tab \
+#                     ${output_tab} --double_img --lr ${lr[k]} &
+#         wait
+#         done
+#     done
+# done
