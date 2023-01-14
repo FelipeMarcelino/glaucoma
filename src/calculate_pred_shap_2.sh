326978e89ac1c18659da72bab1048479b038a4da
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2023-01-14 20:53:54 -0300

    model_summary

--- src/calculate_pred_shap_2.sh
+++ src/calculate_pred_shap_2.sh
@@ -1,3 +1,2 @@
-python main.py --model_id b14f9dd34704488abbb3be82cb9aa124 -oos ../data_oos.csv --shap --balanced_shap --score
-python main.py --model_id 5dc7ecb231344d38b5c4f83dc5e24f95 -oos ../data_oos.csv --shap --balanced_shap --score
-python main.py --model_id b674627d958947d79102841cdd7e2d90 -oos ../data_oos.csv --shap --balanced_shap --score
+python main.py --model_id 86d27a1507a8431aada713f3f2b49a86 -oos ../data_oos.csv --score
+python main.py --model_id 0519fa1f142549d39b448a07fa309107 -oos ../data_oos.csv --score
